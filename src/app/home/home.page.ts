69c4eb1b02e91727efb6ac8c139fbea14fa79a19
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-01-28 19:49:00 +0500

    woring code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  myImage = null;
+
+  serverURL = 'https://someServerUrl.com/imageUplaod'
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt
+    });
+
+    this.myImage = await fetch(image.webPath).then(r => r.blob());
+    if (Capacitor.getPlatform() !== 'web') {
+      this.uploadImages(this.myImage)
+    }
+  }
+
+  async uploadImages(file) {
+    let f = file;
+    f.name = Date.now() + '.' + file.type.split('/')[1];
+    const formData = new FormData();
+    formData.append('picture', f, Date.now() + '.' + file.type.split('/')[1]);
+    this.http.post(this.serverURL, formData).subscribe((res: any) => {
+      console.log(res)
+    })
+
+  }
+
 
 }
