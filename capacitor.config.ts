1758349ba27a4abf17041ab6226693dbbae761e5
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-01-28 18:33:06 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'imgUplaod',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
